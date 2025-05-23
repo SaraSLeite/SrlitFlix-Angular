d0935d97ab1b820f7f2713f7313e773ff90f2acc
SaraSLeite@gmail.com <saraleite2708@gmail.com>
2025-05-23 20:37:45 -0300

    Incluindo um banner na pagina principal

--- srlitflix/src/app/services/movie-api.service.ts
+++ srlitflix/src/app/services/movie-api.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +9,18 @@ export class MovieApiService {
 
   constructor(private http: HttpClient) { }
 
-  
+  baseUrl = "https://api.themoviedb.org/3";
+  options = {
+    method: 'GET',
+    headers: {
+      accept: 'aplication/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMTgzNGY1YjJhNTJkZGIyMmM4MjYwNDRlOTVlMGFmYyIsIm5iZiI6MTc0NzI2NTAzMS40NjMsInN1YiI6IjY4MjUyNjA3YzJkNDA4M2U5MDJkMjczMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.X6s4IJ-Q8G9cgRSfT9fEk7-Cx6HZ5lg7gfHdKJfTlNc'
+    }
+  }
+
+  // Banner de Midias da Semana 
+  bannerApiData(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/trending/all/week?language=pt-br`, this.options);
+  }
 
 }
